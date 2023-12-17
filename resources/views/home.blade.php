a9879273fe1252653686c8f3a5bf27a8bc62513f
lebahao1012 <lebahao10122002@gmail.com>
2023-12-17 22:44:54 +0700

    add1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,78 @@
+@push('css')
+    <link rel="stylesheet" href="{{  asset('/frontend/style/style.css')}}">
+@endpush
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="shortcut icon" href="./assets/images/4375011_logo_netflix_icon.png" type="image/x-icon" />
+    @stack('css')
+    <title>Neflix</title>
+</head>
+
+<body>
+    <div class="container-fluid p-0">
+        <div class="home-background vh-100">
+            <div class="row header px-5 m-0">
+                <div class="d-flex col-8 align-items-center">
+                    <img src="{{ asset('/frontend/images/netflix.png') }}" alt="Logo" width="100" class="me-4" />
+                    <ul class="d-flex m-0">
+                        <li class="list-unstyled me-4 text-white">
+                            <a href="./index.html" class="text-white">Home</a>
+                        </li>
+                        <li class="list-unstyled me-4 text-white">
+                            <a href="./shows.html" class="text-white">TV Shows</a>
+                        </li>
+                        <li class="list-unstyled me-4 text-white">
+                            <a href="./movies.html" class="text-white">Movies</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="d-flex col-4 align-items-center justify-content-end">
+                    <div class="header-icon">
+                        <a href="./search.html"><img src="{{ asset('/frontend/icon/icons8-search-50.png') }}" alt="search-icon"
+                                width="20" height="20" /></a>
+                    </div>
+                    <div class="header-icon ms-3">
+                        <a href="./profile.html">
+                            <img src="{{ asset('/frontend/icon/icons8-person-50.png') }}" alt="person-icon" width="20"
+                                height="20" />
+                        </a>
+                    </div>
+                    <div class="header-icon ms-3">
+                        <a href="./signin.html">
+                            <img src="{{ asset('/frontend/icon/icons8-log-out-26.png') }}" alt="logout-icon" width="20"
+                                height="20" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="row px-5 content-title me-0">
+                <h1 class="text-white fw-bold mb-3">Need for Speed</h1>
+                <p class="text-white fw-bold">Season 1, Episode 1</p>
+                <div class="d-flex">
+                    <button class="btn btn-play col-1 d-flex align-items-center justify-content-center me-3">
+                        <img src="{{ asset('/frontend/icon/icons8-play-24.png" alt="icon-play') }}" />
+                        <p class="text-white p-0 m-0">Play</p>
+                    </button>
+                    <button class="btn btn-mute col- d-flex align-items-center justify-content-center">
+                        <img src="{{ asset('/frontend/icon/icons8-mute-24.png') }}" alt="icon-mute" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [IndexController::class, 'index']);
+
+Route::get('/movies', [IndexController::class, 'movies']);
